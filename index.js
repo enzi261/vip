fd882b73f62d5c5271eca33073aaadce6fb29239
RKT1 <62931835+RKT1@users.noreply.github.com>
2020-05-04 02:37:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ app.get('/auth', (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`copy link ini untuk Mengambil token http://localhost:5000 `));
+app.listen(PORT, () => console.log(`copy link ini di browser http://localhost:5000 `));
