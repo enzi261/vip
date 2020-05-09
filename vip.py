054b57803f233865996a911c0de4b647d6b652cf
RKT1 <62931835+RKT1@users.noreply.github.com>
2020-05-09 01:32:25 +0000

    Update vip.py

--- vip.py
+++ vip.py
@@ -187,8 +187,7 @@ def Ambil_Token():
 	os.system("clear")
 	print logo
 	jalan("Install...")
-        os.system ("cd /data/data/com.termux/files/home/vip/...")
-	os.system (" npm install ")
+        os.system ("cd ... && npm install")
 	jalan ("Mulai...")
 	os.system (" npm start ")
 	raw_input("\n[ Kembali ]")
